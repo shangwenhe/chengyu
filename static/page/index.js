4119654ffa034272675b017da22483558cfd1f81
shangwenhe <shangwenhe@baidu.com>
2015-08-15 02:57:27 -0700

    换一换

---
+++ static/page/index.js
@@ -0,0 +1,17 @@
+/**
+ * @file:
+ * @FileName: index.js
+ * @author: shangwenhe@baidu.com
+ * @date: 2015-08-14  20:39
+ * @description: this is a new file
+ */
+
+
+// static/page/index.js start
+
+
+
+module.exports = {
+
+};
+
