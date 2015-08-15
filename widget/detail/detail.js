7577992b444c389fe3cdce3ff20f07e5e20abadd
shangwenhe <shangwenhe@baidu.com>
2015-08-14 17:24:23 -0700

    config prod

---
+++ widget/detail/detail.js
@@ -0,0 +1,18 @@
+/**
+ * @file:
+ * @FileName: detail.js
+ * @author: shangwenhe@baidu.com
+ * @date: 2015-08-14  17:04
+ * @description: this is a new file
+ */
+
+
+// widget/detail/detail.js start
+
+
+
+module.exports = function(){
+    console.log('=========');
+
+};
+
